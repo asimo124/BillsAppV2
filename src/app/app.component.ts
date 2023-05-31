479ad5c0a58f20b00896862369a1117912be2ec5
alex hawley <alex@engage2learn.org>
2023-05-30 21:10:48 -0500

    - Fixed issue with end of payhecks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,9 @@ export class AppComponent implements OnInit, OnChanges {
     results: any[];
     hash_key: string;
     cur_balance: number;
+    num_days_pay_period: number;
+    remaining_balance: number;
+    pay_date: string;
   };
   billsList: any[];
   numDaysPayPeriod = 1;
@@ -30,6 +33,8 @@ export class AppComponent implements OnInit, OnChanges {
   finalDays = 14;
 
   testMode = false;
+
+  addLastDays = true;
 
   runningTotalBalance: number;
 
@@ -54,28 +59,13 @@ export class AppComponent implements OnInit, OnChanges {
     // get and subscribe to Coachee Growth Benchmarks Download Data
     this.subs.push(this.billsService.billsList.subscribe(response => {
       if (response) {
+
         this.billResults = response;
         this.billsList = response.results;
 
-        //*/
-        const self = this;
-        this.daysLeftCount = 0;
-        this.billsList.forEach(function getWeek(week) {
-          week.days.forEach(function getDay(day) {
-            if (day.showAsDay) {
-              self.daysLeftCount++;
-            }
-          });
-        });
-        //*/
-        this.curBalance = response.cur_balance;
+        this.processResults();
 
-        this.currentDate = null;
-        console.log('response.pay_date: ', response.pay_date);
-        this.currentDate = new Date(response.pay_date);
-        this.numDaysPayPeriod = response.num_days_pay_period;
-        this.remainingBalance = response.remaining_balance;
-        console.log('billsList: ', this.billsList);
+
       }
     }));
 
@@ -88,6 +78,74 @@ export class AppComponent implements OnInit, OnChanges {
 
   onChange(event) {
     console.log('event: ', event.toLocaleString());
+  }
+
+  updateLastDays() {
+    this.processResults();
+  }
+
+  processResults() {
+
+    //*/
+    const self = this;
+    this.daysLeftCount = 0;
+    this.billsList.forEach(function getWeek(week) {
+      week.days.forEach(function getDay(day) {
+        if (day.showAsDay) {
+          self.daysLeftCount++;
+        }
+      });
+    });
+    //*/
+    this.curBalance = this.billResults.cur_balance;
+
+    this.currentDate = null;
+    this.currentDate = new Date(this.billResults.pay_date);
+    this.numDaysPayPeriod = this.billResults.num_days_pay_period;
+
+    const date2 = new Date();
+    const currentDay = date2.getDate();
+    const currentMonth = date2.getMonth() + 1;
+    if (this.addLastDays) {
+      if (currentDay > 25) {
+
+        let firstMonth = null;
+        let startsFirst = false;
+        let i = 0;
+        this.billsList.forEach(function getWeek(week) {
+          week.days.forEach(function getDay(day) {
+            if (day.showAsDay) {
+              if (i === 0) {
+                if (day.Day.indexOf(', 1st') > -1) {
+                  const firstDate = day.Date;
+                  firstMonth = parseInt(firstDate.split('\/')[0], 10);
+                  startsFirst = true;
+                }
+              }
+              i++;
+            }
+          });
+        });
+        let nextMonth = null;
+        if (startsFirst) {
+          let numDaysAdd = 0;
+          while (true) {
+            numDaysAdd += 1;
+            date2.setDate(date2.getDate() + 1);
+            if (date2.getDate() === 1) {
+              nextMonth = date2.getMonth() + 1;
+              break;
+            }
+          }
+          if (firstMonth - 1 === currentMonth) {
+            this.numDaysPayPeriod += numDaysAdd;
+          }
+          console.log('numDaysAdd: ', numDaysAdd);
+        }
+      }
+    }
+    this.remainingBalance = this.billResults.remaining_balance;
+    console.log('billsList: ', this.billsList);
   }
 
   loadPrevDate() {
